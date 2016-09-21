48252d75b3d9aa5f827246bdbe1d5f0ef82dda69
Leandro <lealuque.tw@gmail.com>
2016-09-21 15:58:58 -0300

    Muestra la data que se envia

---
+++ serverImpl-02.js
@@ -0,0 +1,27 @@
+var net = require('net');
+
+var server = net.createServer();
+
+server.on('connection', function (sock){
+	console.log('Nueva conexion');
+	console.log('Socket remoto: '+ sock.remoteAddress + ' ' + sock.remotePort);
+	console.log('Socket local: '+ sock.localAddress + ' ' + sock.localPort);
+
+	sock.setEncoding('utf8');
+
+	sock.on('data', function (data) {
+		process.stdout.write("Datos recibidos: " + data);
+	});
+
+	sock.on('end', function (){
+		console.log('FIn del socket: ' + sock.remoteAddress + ' ' + sock.remotePort);
+	});
+
+	sock.once('close', function (data) {
+		console.log('Socket cerrado: ' + sock.remoteAddress + ' ' + sock.remotePort);
+	});
+
+});
+
+
+module.exports = server;
