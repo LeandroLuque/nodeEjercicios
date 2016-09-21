90a8facf1fa5d73e652b25aedf9dadea1fb380a2
Leandro <lealuque.tw@gmail.com>
2016-09-21 16:30:09 -0300

    Ejercicio 2

---
+++ serverImpl-04.3.1.js
@@ -0,0 +1,36 @@
+var net = require('net');
+var through = require('through2');
+
+var server = net.createServer();
+
+server.on('connection', function (sock){
+	console.log('Nueva conexion');
+	console.log('Socket remoto: '+ sock.remoteAddress + ' ' + sock.remotePort);
+	console.log('Socket local: '+ sock.localAddress + ' ' + sock.localPort);
+
+	sock.setEncoding('utf8');
+
+
+	var upperCasing = through.obj(function (str, enc, cb) {
+		this.push(str.toUpperCase());
+		cb();
+	});
+
+	sock.pipe(upperCasing).pipe(sock);
+
+	sock.on('data', function (data) {
+		console.log('Data: ',data);
+	});
+
+	sock.on('end', function (){
+		console.log('FIn del socket: ' + sock.remoteAddress + ' ' + sock.remotePort);
+	});
+
+	sock.once('close', function (data) {
+		console.log('Socket cerrado: ' + sock.remoteAddress + ' ' + sock.remotePort);
+	});
+
+});
+
+
+module.exports = server;
